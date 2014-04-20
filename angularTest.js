a1e683e80deb934f9d89a4b3af55a09d830ba844
trey <treygeorge@gmail.com>
2014-04-20 15:27:06 -0500

    Adding follows functionality.  Adding bootstrap styles.  Minor improvements.

--- angularTest.js
+++ angularTest.js
@@ -3,20 +3,28 @@ angular.module('angularTest', ['ngRoute'])
 .value('twitchApi', 'https://api.twitch.tv/kraken')
 
 .config(function($routeProvider) {
-  $routeProvider
-  .when('/videos', {
-      controller:'VideosController'
+    $routeProvider
+    .when('/videos', {
+        controller:'VideosController',
+        templateUrl:'views/videos.html'
+    })
+    .when('/follows', {
+        controller: 'FollowsController',
+        templateUrl:'views/follows.html'
     })
     .otherwise({
-      redirectTo:'/videos'
+        redirectTo:'/videos'
     });
 })
 
 .factory('videoService', function($http, twitchApi){
 	var videoService = {
-		getVideos : function(userName){
+		getVideos : function(userName) {
 	 		return $http.jsonp(twitchApi + '/channels/' + userName + '/videos?limit=10' + '&callback=JSON_CALLBACK');
-	 	}
+		},
+		getFollows : function(userName) {
+			return $http.jsonp(twitchApi + '/users/' + userName + '/follows/channels?limit=10' + '&callback=JSON_CALLBACK');
+		}
 	};
 
 	return videoService;
@@ -24,11 +32,11 @@ angular.module('angularTest', ['ngRoute'])
 
 .controller('VideosController', function($scope, $routeParams, twitchApi, $http, videoService) {
 
-	if($routeParams.userName == undefined)
-		$routeParams.userName = 'wingsofdeath';
+	if($scope.userName == undefined)
+		$scope.userName = 'wingsofdeath';
 
 	function init(){
-		videoService.getVideos($routeParams.userName)
+		videoService.getVideos($scope.userName)
 			.success(function(data){
 				$scope.videos = data.videos;
 			});
@@ -36,21 +44,36 @@ angular.module('angularTest', ['ngRoute'])
 
 	init();
 })
-.directive('searchButton', function(videoService){
+
+.controller('FollowsController', function($scope, $routeParams, $http, videoService) {
+	if($scope.userName == undefined)
+		$scope.userName = 'wingsofdeath';
+
+	function init() {
+		videoService.getFollows($scope.userName)
+			.success(function(data) {
+				$scope.follows = data.follows;
+			});
+	}
+
+	init();
+})
+
+.directive('searchVideosButton', function(videoService){
 
 	return {
 		restrict: 'EA',
 		replace: false,
-		templateUrl: 'views/updateVideosButton.html',
+		templateUrl: 'directives/updateVideosButton.html',
 		link: function(scope, $element) {
 
-			var getVideos = function getVideos(){
+			var getVideos = function() {
 				var searchName = scope.searchName;
 				videoService.getVideos(searchName)
 					.then(
 						function(response){
 							scope.videos = response.data.videos;
-							scope.searchName = searchName;
+							scope.userName = searchName;
 						});
 			}
 
@@ -62,13 +85,27 @@ angular.module('angularTest', ['ngRoute'])
 		}
 	}
 })
-.directive('videos', function(videoService){
-	console.log('videos directive');
+.directive('searchFollowsButton', function(videoService) {
 	return {
 		restrict: 'EA',
 		replace: false,
-		templateUrl: 'views/videos.html',
-		link: function(scope) {
+		templateUrl: 'directives/updateVideosButton.html',
+		link: function(scope, $element) {
+			var getFollows = function() {
+				var searchName = scope.searchName;
+				videoService.getFollows(searchName)
+					.then(
+						function(response){
+							scope.follows = response.data.follows;
+                            scope.userName = searchName;
+					})
+			}
+
+			$element.on('click', function() {
+				scope.$apply(getFollows);
+			});
+
+			scope.updateFollows = getFollows;
 		}
-	}	
+	}
 });
